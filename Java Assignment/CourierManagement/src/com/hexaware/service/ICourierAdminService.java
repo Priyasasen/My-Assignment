1a860ff3e14d089fbe93a1f2d9086037b9343790
Priyasasen <priyasasen23@gmail.com>
2025-04-15 20:46:50 +0530

    Deleted Java Assignment folder

--- Java Assignment/CourierManagement/src/com/hexaware/service/ICourierAdminService.java
+++
@@ -1,7 +0,0 @@
-package com.hexaware.service;
-
-//In ICourierAdminService.java
-public interface ICourierAdminService {
- int addCourierStaff(String name, String contactNumber); // matching the expected parameters
-}
-
